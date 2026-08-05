ac46bb86ce8cc37cd30328fcd24dc031f8726ec9
lguzman07 <laurapguzmann@gmail.com>
2026-08-05 02:32:43 -0400

    changes in order why us

--- components/WhyDifferent.tsx
+++ components/WhyDifferent.tsx
@@ -13,16 +13,24 @@ export default function WhyDifferent() {
       icon: '🌎',
       title: 'Metodología diseñada por y para hispanohablantes',
       description:
-        'Aprender inglés debe ser una experiencia gratificante; no frustrante. En ocasiones, se espera que conozcas el idioma incluso antes de empezar a hablarlo. Es por eso que he diseñado un espacio específicamente para hispanohablantes, donde se explican los conceptos a partir de tu lengua materna, pensando en las posibles preguntas o dificultades que puedas tener.', 
-      },
+        'Aprender inglés debe ser una experiencia gratificante; no frustrante. En ocasiones, se espera que conozcas el idioma incluso antes de empezar a hablarlo. Es por eso que he diseñado un espacio específicamente para hispanohablantes, donde se explican los conceptos a partir de tu lengua materna, pensando en las posibles preguntas o dificultades que puedas tener.',
+    },
 
-          {
+    {
+      icon: '🧠',
+      title: 'Te enseño inglés, no a usar una plataforma confusa',
+      description:
+        'He diseñado una plataforma intuitiva y fácil de usar. Accede a todas las reuniones bajo un mismo enlace. Cada lección en PDF viene con su hoja de respuestas incluida.Las lecciones son cortas, con botones grandes, y explicaciones incluidas en la sección de cada ejercicio. Concéntrate en aprender inglés, no en cómo usar la plataforma.',
+    },
+
+
+    {
       icon: '🌱',
       title: 'Aprendizaje pausado',
       description:
         'No hay niveles que desbloquear, vidas que perder, ni rankings que alcanzar. Nuestra filosofía se basa en aprender de una manera calmada, a tu ritmo, y compitiendo solamente contigo mismo.',
     },
-        {
+    {
       icon: '🛣️',
       title: 'Las rutas son una guía, no un camino obligatorio',
       description:
@@ -33,14 +41,6 @@ export default function WhyDifferent() {
       title: 'Asistencia en español al correo electrónico',
       description:
         'De tener preguntas sobre las lecciones puedes escribirnos al correo lau@inglesconlau.com y recibirás asistencia en español. No necesitas escribir en inglés para recibir ayuda.',
-    },
-
-
-        {
-      icon: '🧠', 
-      title: 'Te enseño inglés, no a usar una plataforma confusa',
-      description:
-        'He diseñado una plataforma intuitiva y fácil de usar. Accede a todas las reuniones bajo un mismo enlace. Cada lección en PDF viene con su hoja de respuestas incluida.Las lecciones son cortas, con botones grandes, y explicaciones incluidas en la sección de cada ejercicio. Concéntrate en aprender inglés, no en cómo usar la plataforma.',
     },
 
   ];
