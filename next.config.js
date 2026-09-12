4d2f3cbf0a88753f2155d8cd334ce23574ee3dbe
lguzman07 <122062670+lguzman07@users.noreply.github.com>
2026-09-12 01:19:14 -0400

    Add rewrites for clase-recordatorio endpoint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/clase-recordatorio',
+        destination: 'https://cheefifbhpmvfxkumwdh.supabase.co/functions/v1/class-actions',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
